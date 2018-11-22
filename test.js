eaafef3076b2bf6d0c57cc1ce8368f97a7c4899d
Bart Veneman <bartveneman@users.noreply.github.com>
2018-11-22 22:28:09 +0100

    init

--- test.js
+++ test.js
@@ -2,93 +2,57 @@ const test = require('ava')
 const isBrowserHack = require('.')
 
 test('It recognizes browser hacks successfully', t => {
-	t.true(isBrowserHack('@supports (-webkit-appearance:none)'))
-	t.true(isBrowserHack('@media \\\\0 screen'))
-
 	// Firefox
-	t.true(isBrowserHack('@media \\0 all'))
-	t.true(isBrowserHack('@media screen and (-moz-images-in-menus:0)'))
-	t.true(isBrowserHack('@media screen and (min--moz-device-pixel-ratio:0)'))
+	t.true(isBrowserHack('\\0 all'))
+	t.true(isBrowserHack('screen and (-moz-images-in-menus:0)'))
+	t.true(isBrowserHack('screen and (min--moz-device-pixel-ratio:0)'))
 	t.true(
 		isBrowserHack(
-			'@media all and (min--moz-device-pixel-ratio:0) and (min-resolution: .001dpcm)'
+			'all and (min--moz-device-pixel-ratio:0) and (min-resolution: .001dpcm)'
 		)
 	)
 	t.true(
 		isBrowserHack(
-			'@media all and (-moz-images-in-menus:0) and (min-resolution: .001dpcm)'
+			'all and (-moz-images-in-menus:0) and (min-resolution: .001dpcm)'
 		)
 	)
-	t.true(isBrowserHack('@media screen and (min-width:0\\0)'))
+	t.true(isBrowserHack('screen and (min-width:0\\0)'))
 	t.true(
 		isBrowserHack(
-			'@media screen and (-ms-high-contrast: active), (-ms-high-contrast: none)'
-		)
-	)
-	t.true(isBrowserHack('@-moz-document url-prefix()'))
-	t.true(isBrowserHack('@supports (-moz-appearance:meterbar)'))
-	t.true(
-		isBrowserHack('@supports (-moz-appearance:meterbar) and (display:flex)')
-	)
-	t.true(
-		isBrowserHack('@supports (-moz-appearance:meterbar) and (cursor:zoom-in)')
-	)
-	t.true(
-		isBrowserHack(
-			'@supports (-moz-appearance:meterbar) and (background-attachment:local)'
+			'screen and (-ms-high-contrast: active), (-ms-high-contrast: none)'
 		)
 	)
 	t.true(
 		isBrowserHack(
-			'@supports (-moz-appearance:meterbar) and (image-orientation:90deg)'
+			'all and (min--moz-device-pixel-ratio:0) and (min-resolution: 3e1dpcm)'
 		)
 	)
-	t.true(
-		isBrowserHack('@supports (-moz-appearance:meterbar) and (all:initial)')
-	)
-	t.true(
-		isBrowserHack(
-			'@supports (-moz-appearance:meterbar) and (list-style-type:japanese-formal)'
-		)
-	)
-	t.true(
-		isBrowserHack(
-			'@media all and (min--moz-device-pixel-ratio:0) and (min-resolution: 3e1dpcm)'
-		)
-	)
-	t.true(
-		isBrowserHack(
-			'@supports (-moz-appearance:meterbar) and (background-blend-mode:difference,normal)'
-		)
-	)
-	t.true(isBrowserHack('@-moz-document url-prefix()'))
 
 	// IE / Edge
-	t.true(isBrowserHack('@media screen\\9'))
-	t.true(isBrowserHack('@media \\0screen\\,screen\\9'))
-	t.true(isBrowserHack('@media \\0screen'))
-	t.true(isBrowserHack('@media screen and (min-width:0\\0)'))
+	t.true(isBrowserHack('screen\\9'))
+	t.true(isBrowserHack('\\0screen\\,screen\\9'))
+	t.true(isBrowserHack('\\0screen'))
+	t.true(isBrowserHack('screen and (min-width:0\\0)'))
 	t.true(
 		isBrowserHack(
-			'@media screen and (-ms-high-contrast: active), (-ms-high-contrast: none)'
+			'screen and (-ms-high-contrast: active), (-ms-high-contrast: none)'
 		)
 	)
 
 	// Opera
 	t.true(
 		isBrowserHack(
-			'@media all and (-webkit-min-device-pixel-ratio:10000), not all and (-webkit-min-device-pixel-ratio:0)'
+			'all and (-webkit-min-device-pixel-ratio:10000), not all and (-webkit-min-device-pixel-ratio:0)'
 		)
 	)
 	t.true(
 		isBrowserHack(
-			'@media all and (-webkit-min-device-pixel-ratio:0) and (min-resolution: .001dpcm)'
+			'all and (-webkit-min-device-pixel-ratio:0) and (min-resolution: .001dpcm)'
 		)
 	)
 })
 
-test('It correctly marks regular atrules as non-hacks', t => {
-	t.false(isBrowserHack('@media all'))
-	t.false(isBrowserHack('@media (min-width: 1px)'))
-	t.false(isBrowserHack('@supports (display: grid)'))
+test('It correctly marks regular media queries as non-hacks', t => {
+	t.false(isBrowserHack('all'))
+	t.false(isBrowserHack('(min-width: 1px)'))
 })
